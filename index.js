4586b23a4738c6edac260c87ed27b0ad86ff1e56
bpaksoy <paksoybanu83@gmail.com>
2017-10-12 14:16:18 -0400

    index.js added

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const env = "development";
 const config = require("./knexfile.js")[env];
 const knex = require("knex")(config)
+
+knex("car").then(function(result) {
+ console.log(result);
+ knex.destroy();
+})
+.catch(function(err) {
+  console.log(err);
+  knex.destroy();
+  process.exit(1);
+})
